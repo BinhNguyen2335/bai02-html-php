0990a2f112a9006e6c4f4ffe6ffe94c54d3db569
BinhNguyen2335 <100409700+BinhNguyen2335@users.noreply.github.com>
2022-02-25 16:20:22 +0700

    Add files via upload

---
+++ bai02-html-php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai02 - Html va PHP</title>
+</head>
+<body>
+    <!-- Header -->
+    <?php include_once './inc/header.php' ?>
+    <h1>Html va PHP</h1>
+    <?php echo 'Hello World'; ?>
+    <?php 
+      $language  = 'Ngon ngu lap trinh PHP';
+      var_dump ($language);
+     ?> 
+     <?php include './inc/content.php'; ?>
+      <!-- Footer -->
+      <?php include_once './inc/footer.php' ?>  
+</body>
+</html>
